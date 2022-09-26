d145fcbea6e37f3959a58658eda5476695b38ed5
lihaohao <2495041749@qq.com>
2022-09-26 18:14:09 +0800

    feat: 初步解析 commander参数

--- src/hooks/useOption.ts
+++ src/hooks/useOption.ts
@@ -1,4 +1,56 @@
 import * as process from "process";
+import { program } from "commander";
+
+const defaultOptions = {
+  ignore: "node_modules",
+  ext: ".vue,.js,.tsx,.ts",
+  fix: false,
+  target: "",
+  visible: true
+};
+type IDefaultOptions = typeof defaultOptions;
+
+type IResultOptions = Omit<IDefaultOptions, "ignore" | "ext"> & {
+  ignore: string[];
+  ext: string[];
+};
+
+/**
+ * @author lihh
+ * @description 获取脚手架中参数
+ */
 export async function useOption() {
-    console.log(process.cwd());
+  const { npm_package_version = "1.0.0" } = process.env;
+  defaultOptions.target = process.cwd();
+
+  program
+    .version(npm_package_version)
+    .option(
+      "-i, --ignore <ignore>",
+      "ignore file or folder, default: ['node_modules']"
+    )
+    .option(
+      "-e, --ext <ext>",
+      "check file suffix, default: ['.vue', '.js', '.tsx', '.ts']"
+    )
+    .option("-f, --fix", "auto remove surplus css")
+    .option("-v, --visible", "print real-time log")
+    .option("-t, --target <target>", "check style Start Folder");
+
+  program.parse(process.argv);
+
+  const options = Object.assign(
+    {},
+    defaultOptions,
+    program.opts()
+  ) as IDefaultOptions;
+
+  const result: IResultOptions = {
+    ignore: options.ignore.split(","),
+    fix: options.fix,
+    target: options.target,
+    ext: options.ext.split(","),
+    visible: options.visible
+  };
+  return result;
 }
